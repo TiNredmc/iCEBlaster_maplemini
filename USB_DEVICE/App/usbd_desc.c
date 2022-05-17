f7d4b4b6f5d8c4caba7cf7e2f96b9030389f2624
Tin LetHax <thipok17@gmail.com>
2022-05-17 08:49:25 +0700

    Updated USB product name.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -66,7 +66,7 @@
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
 #define USBD_PID_FS     22314
-#define USBD_PRODUCT_STRING_FS     "STM32 Mass Storage"
+#define USBD_PRODUCT_STRING_FS     "iCEBlaster"
 #define USBD_CONFIGURATION_STRING_FS     "MSC Config"
 #define USBD_INTERFACE_STRING_FS     "MSC Interface"
 
